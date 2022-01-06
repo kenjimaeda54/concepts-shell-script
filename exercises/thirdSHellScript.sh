1f580cf0aeaca3cc7dfe9ec26df010e9edf65534
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-01-06 17:46:52 -0300

    feat: finished third shell

--- exercises/thirdSHellScript.sh
+++ exercises/thirdSHellScript.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+echo "==========================================================="
+# Precisa de um parametro para executar o script
+#./thirdSHellScript.sh <nome do usuario>
+echo "==========================================================="
+ls /home/$1 > /dev/null 2>&1 || { echo "Usuario nao existe"; exit 1; }
+
+USERID=$(grep $1 /etc/passwd| cut -d":" -f3)
+DESCRIPTION=$(grep kenji /etc/passwd| cut -d":" -f5| tr -d ",")
+
 
 echo
 echo "==================================="
-echo "Relatorio do  Usuario: "
+echo "Relatorio do  Usuario: $1"
 echo ""
-echo "UID: $1"
-echo "Nome ou descricao"
+echo "UID: $USERID"
+echo "Nome ou descricao: $DESCRIPTION"
 echo ""
-echo "Total usado no"
 echo "Ultimo Login"
-
+lastlog -u $1
+echo "==================================="
 
