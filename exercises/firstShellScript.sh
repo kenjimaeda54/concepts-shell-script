52653b9d78ed6847ee8b1b5ee4b26a9b68470477
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-01-06 17:02:39 -0300

    feat: finished first exercise

---
+++ exercises/firstShellScript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+echo "==================================="
+echo "Crie um Script que após executado solicite dois valores como entrada. Esses
+valores devem ser somados e o resultado exibido para o usuário"
+read -p "Digite um valor: " valor1
+read -p "Digite outro valor: " valor2
+echo "O resultado da soma é: " $(expr $valor1 + $valor2)
